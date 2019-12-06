b5a99ac3ab300bd372d5ae1e96f44879ae0cf4e8
Sal <m6567fc@outlook.com>
2019-12-06 11:37:04 +0800

    add writer

--- sunflower-core/src/main/java/org/tdf/sunflower/console/Console.java
+++ sunflower-core/src/main/java/org/tdf/sunflower/console/Console.java
@@ -7,9 +7,11 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.DataListener;
 import org.springframework.stereotype.Component;
 
-import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
 import java.util.UUID;
 
 // Command line interface
@@ -17,11 +19,42 @@ import java.util.UUID;
 public class Console {
     private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
     private final ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
-
     // event for
     private static final String INPUT_EVENT = "console-in";
     private static final String OUTPUT_EVENT = "console-out";
+    private Writer errorWriter = new Writer() {
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            writeError(new String(Arrays.copyOfRange(cbuf, off, off + len)));
+        }
 
+        @Override
+        public void flush() throws IOException {
+
+        }
+
+        @Override
+        public void close() throws IOException {
+
+        }
+    };
+
+    private Writer outWriter = new Writer() {
+        @Override
+        public void write(char[] cbuf, int off, int len) throws IOException {
+            writeOutPut(new String(Arrays.copyOfRange(cbuf, off, off + len)));
+        }
+
+        @Override
+        public void flush() throws IOException {
+
+        }
+
+        @Override
+        public void close() throws IOException {
+
+        }
+    };
 
     private SocketIOServer socketIOServer;
     private String uuid = UUID.randomUUID().toString();
@@ -35,7 +68,9 @@ public class Console {
             // if session id is not valid, disconnect
         });
 
-
+        nashorn.getContext().setWriter(outWriter);
+        nashorn.getContext().setErrorWriter(errorWriter);
+        
         socketIOServer.addEventListener(INPUT_EVENT, ConsoleIn.class, new DataListener<ConsoleIn>() {
             @Override
             public void onData(SocketIOClient client, ConsoleIn data, AckRequest ackSender) throws Exception {
