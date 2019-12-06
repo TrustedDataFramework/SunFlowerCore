6ff59b4cea367d6bb0e3584d54edd4292aa68424
Sal <m6567fc@outlook.com>
2019-12-06 17:01:19 +0800

    regenerate salt for a period

--- sunflower-core/src/main/java/org/tdf/sunflower/console/Console.java
+++ sunflower-core/src/main/java/org/tdf/sunflower/console/Console.java
@@ -4,6 +4,7 @@ import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import io.netty.util.internal.StringUtil;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.tdf.common.HexBytes;
 import org.tdf.crypto.HashFunctions;
@@ -17,18 +18,22 @@ import java.nio.charset.StandardCharsets;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.UUID;
 
 // Command line interface
 @Component
 public class Console {
+    static final SecureRandom SECURE_RANDOM = new SecureRandom();
+    static final int SALT_LENGTH = 8;
     private final ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
     private final ScriptEngine nashorn = scriptEngineManager.getEngineByName("nashorn");
     // event for
     private static final String INPUT_EVENT = "console-in";
     private static final String OUTPUT_EVENT = "console-out";
     private ConsoleConfig consoleConfig;
+    private String salt;
 
     private Writer errorWriter = new Writer() {
         @Override
@@ -99,6 +104,28 @@ public class Console {
     private void regenerateUUID() {
         uuid = UUID.randomUUID().toString();
         writeTokenFile();
+    }
+    
+    public void regenerateSalt() {
+        int[] saltInts = new int[SALT_LENGTH];
+        for (int i = 0; i < saltInts.length; i++) {
+            while (true) {
+                int j = SECURE_RANDOM.nextInt('z');
+                if (isReadAble(j)) {
+                    saltInts[i] = j;
+                    break;
+                }
+            }
+        }
+        byte[] saltBytes = new byte[SALT_LENGTH];
+        for (int i = 0; i < saltBytes.length; i++) {
+            saltBytes[i] = (byte) saltInts[i];
+        }
+        salt = new String(saltBytes, StandardCharsets.US_ASCII);
+    }
+
+    private boolean isReadAble(int i) {
+        return ('0' <= i && i <= '9') || ('a' <= i && i <= 'z') || ('A' <= i && i <= 'Z');
     }
 
     // read port, generate uuid write to token file
