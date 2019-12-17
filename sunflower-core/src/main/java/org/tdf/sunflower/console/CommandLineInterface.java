d59b5108ebe693129285dcdac6702f14c63ed27c
Sal <m6567fc@outlook.com>
2019-12-17 12:40:36 +0800

    Update CommandLineInterface.java

--- sunflower-core/src/main/java/org/tdf/sunflower/console/CommandLineInterface.java
+++ sunflower-core/src/main/java/org/tdf/sunflower/console/CommandLineInterface.java
@@ -1,14 +1,17 @@
 package org.tdf.sunflower.console;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
+@ConditionalOnProperty(value = "sunflower.console.disabled", havingValue = "false")
 @Slf4j
 public class CommandLineInterface {
 
-    @ShellMethod(value = "shutdown")
+    @ShellMethod(value = "shutdown", key = "shutdown")
     public void shutdown() {
         log.info("application will shutdown soon");
         System.exit(0);
