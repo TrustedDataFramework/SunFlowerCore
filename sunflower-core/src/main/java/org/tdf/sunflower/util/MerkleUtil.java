3fb64c717a11a9aebd0857e754396664a46fa5de
Sal <m6567fc@outlook.com>
2020-01-13 15:16:56 +0800

    add mekle util for merkel root calc

---
+++ sunflower-core/src/main/java/org/tdf/sunflower/util/MerkleUtil.java
@@ -0,0 +1,29 @@
+package org.tdf.sunflower.util;
+
+import org.tdf.common.serialize.Codec;
+import org.tdf.common.store.ByteArrayMapStore;
+import org.tdf.common.trie.Trie;
+import org.tdf.common.types.Hashed;
+import org.tdf.rlp.RLPCodec;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+public class MerkleUtil {
+    public static byte[] getMerkleRoot(Collection<? extends Hashed> nodes, Function<byte[], byte[]> hashFunction) {
+        int i = 0;
+
+        Trie<Integer, byte[]> trie = Trie.<Integer, byte[]>builder()
+                .hashFunction(hashFunction)
+                .keyCodec(Codec.newInstance(RLPCodec::encodeInt, RLPCodec::decodeInt))
+                .valueCodec(Codec.identity())
+                .store(new ByteArrayMapStore<>())
+                .build();
+
+        for(Hashed n: nodes){
+            trie.put(i, n.getHash().getBytes());
+            i++;
+        }
+        return trie.commit();
+    }
+}
