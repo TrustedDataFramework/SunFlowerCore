084e96692c697cf894fe687ba49156f8f6a07114
zhuyingjie <m6567fc@outlook.com>
2020-03-12 00:04:51 +0800

    Update BugTest.java

--- sunflower-core/src/test/java/org/tdf/sunflower/BugTest.java
+++ sunflower-core/src/test/java/org/tdf/sunflower/BugTest.java
@@ -48,11 +48,11 @@ public class BugTest {
         String[] pks = new String[]{
                 "02b507fe1afd0cc7a525488292beadbe9f143784de44f8bc1c991636509fd50936",
                 "02f9d915954e04107d11fb9689a6330c22199e1e830857bff076e033bbca2888d4",
-//                "03cd2875d6381b974bd13c9c6087c08fcf0b9b700ea8c9b601ae35a6a9651fbce2",
-//                "03744f32e35e8e45cfa6360b49fe12e730cb294bce40db5099b0de697aa00a3d71",
-//                "020f0a0c7fb839a51b1f64cf2b49f9b3269d2b9ca49d3309e3de4453f389c827bb",
-//                "02281b065a508bace266556f239e491a44a0a64789c7d4fc333d2a875a7ddc1714",
-//                "039608d098f275db24db04211d62f66e1438bdd0d93d7e3dbe33136ef3fc53c726"
+                "03cd2875d6381b974bd13c9c6087c08fcf0b9b700ea8c9b601ae35a6a9651fbce2",
+                "03744f32e35e8e45cfa6360b49fe12e730cb294bce40db5099b0de697aa00a3d71",
+                "020f0a0c7fb839a51b1f64cf2b49f9b3269d2b9ca49d3309e3de4453f389c827bb",
+                "02281b065a508bace266556f239e491a44a0a64789c7d4fc333d2a875a7ddc1714",
+                "039608d098f275db24db04211d62f66e1438bdd0d93d7e3dbe33136ef3fc53c726"
         };
         int iters = 100;
         int count = 10;
