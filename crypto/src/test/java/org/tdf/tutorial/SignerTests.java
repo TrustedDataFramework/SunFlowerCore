c8c4de788ef6256abd86b651cd7fbe0735f92167
zhuyingjie <m6567fc@outlook.com>
2020-06-22 14:37:08 +0800

    add ecdsa signer

--- crypto/src/test/java/org/tdf/tutorial/SignerTests.java
+++ crypto/src/test/java/org/tdf/tutorial/SignerTests.java
@@ -3,10 +3,9 @@ package org.tdf.tutorial;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.tdf.crypto.tutorial.DSASigner;
-import org.tdf.crypto.tutorial.ElgamalSigner;
-import org.tdf.crypto.tutorial.SchnorrSigner;
+import org.tdf.crypto.tutorial.*;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
@@ -22,6 +21,7 @@ public class SignerTests {
         long[] sig = signer.sign(sk, x);
         System.out.println(sig[0] + "  " + sig[1]);
         assertTrue(signer.verify(sig, x, beta));
+        assertFalse(signer.verify(sig, x + 1, beta));
     }
 
     // Schnorr 签名方案
@@ -34,7 +34,8 @@ public class SignerTests {
         long beta = signer.getBeta(sk);
         long x = 1000;
         long[] sig = signer.sign(sk, x);
-        assertTrue(signer.verify(sig, 1000, beta));
+        assertTrue(signer.verify(sig, x, beta));
+        assertFalse(signer.verify(sig, x + 1, beta));
     }
 
     // DSA 签名方案
@@ -48,5 +49,18 @@ public class SignerTests {
         byte[] msg = "123".getBytes();
         long[] sig = signer.sign(sk, msg);
         assertTrue(signer.verify(sig, msg, beta));
+        assertFalse(signer.verify(sig, "12".getBytes(), beta));
+    }
+
+    // ECDSA 签名方案
+    @Test
+    public void test4() {
+        ECDSASigner signer =
+                new ECDSASigner(11, 13, 1, 6, new ECCPoint(2, 7));
+
+        long sk = 7;
+        long[] sig = signer.sign(sk, "123".getBytes());
+        assertTrue(signer.verify(sig, "123".getBytes(), signer.getPublicKey(sk)));
+        assertFalse(signer.verify(sig, "12".getBytes(), signer.getPublicKey(sk)));
     }
 }
