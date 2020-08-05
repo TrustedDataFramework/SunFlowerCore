0b8187fa85302130d9f1957c7c3fd1c7853ca6bc
zhuyingjie <m6567fc@outlook.com>
2020-08-05 15:46:11 +0800

    Update GasLimit.java

--- sunflower-core/src/main/java/org/tdf/sunflower/vm/hosts/GasLimit.java
+++ sunflower-core/src/main/java/org/tdf/sunflower/vm/hosts/GasLimit.java
@@ -1,6 +1,5 @@
 package org.tdf.sunflower.vm.hosts;
 
-import lombok.Getter;
 import org.tdf.lotusvm.runtime.Hook;
 import org.tdf.lotusvm.runtime.HostFunction;
 import org.tdf.lotusvm.runtime.ModuleInstanceImpl;
@@ -8,18 +7,21 @@ import org.tdf.lotusvm.types.Instruction;
 import org.tdf.sunflower.ApplicationConstants;
 
 public class GasLimit implements Hook {
-    @Getter
-    private long gas;
+    private long steps;
 
     @Override
     public void onInstruction(Instruction ins, ModuleInstanceImpl module) {
-        gas++;
-        if(gas > ApplicationConstants.GAS_LIMIT) throw new RuntimeException("gas overflow");
+        steps++;
+        if(getGas() > ApplicationConstants.GAS_LIMIT) throw new RuntimeException("gas overflow");
     }
 
     @Override
     public void onHostFunction(HostFunction function, ModuleInstanceImpl module) {
-        gas++;
-        if(gas > ApplicationConstants.GAS_LIMIT) throw new RuntimeException("gas overflow");
+        steps++;
+        if(getGas() > ApplicationConstants.GAS_LIMIT) throw new RuntimeException("gas overflow");
+    }
+
+    public long getGas(){
+        return steps / 1024;
     }
 }
