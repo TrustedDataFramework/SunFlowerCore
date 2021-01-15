8ec7719c6b7dd5c8e92703b875c3b62aae9ad25c
zhuyingjie <m6567fc@outlook.com>
2021-01-15 21:14:57 +0800

    Update EpochSecondsSerializer.java

--- common/src/main/java/org/tdf/common/util/EpochSecondsSerializer.java
+++ common/src/main/java/org/tdf/common/util/EpochSecondsSerializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
@@ -14,11 +15,11 @@ import java.util.concurrent.TimeUnit;
  * convert unix epoch seconds to "2011-12-03T10:15:30+01:00" as like
  */
 public class EpochSecondsSerializer extends JsonSerializer<Long> {
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneOffset.UTC);
+
     @Override
     public void serialize(Long value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        ZoneId zone = ZoneId.systemDefault();
-        DateTimeFormatter df = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(zone);
-        gen.writeString(df.format(Instant.ofEpochMilli(
+        gen.writeString(FORMATTER.format(Instant.ofEpochMilli(
                 TimeUnit.MILLISECONDS.convert(value, TimeUnit.SECONDS))
         ));
     }
