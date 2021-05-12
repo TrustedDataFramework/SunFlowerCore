4be4cd2c0f4219a4128a21eca200ce364a3523ed
zhuyingjie <m6567fc@outlook.com>
2021-05-12 15:14:12 +0800

    Update RcStackResource.kt

--- facade/src/main/java/org/tdf/sunflower/vm/RcStackResource.kt
+++ facade/src/main/java/org/tdf/sunflower/vm/RcStackResource.kt
@@ -5,6 +5,7 @@ import org.tdf.lotusvm.runtime.StackAllocator
 class RcStackResource (val p: StackAllocator, private var refers: Int = 0): StackAllocator by p, StackResource{
     override fun close() {
         refers --
+        this.p.clear()
     }
 
     fun refer() {
