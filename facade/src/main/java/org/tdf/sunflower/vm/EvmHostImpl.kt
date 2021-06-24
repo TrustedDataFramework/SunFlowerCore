63e3e79171bbe3f2647f503fae13c3063cf89638
sal <sal@salpadding.local>
2021-06-24 09:24:54 +0800

    updates

---
+++ facade/src/main/java/org/tdf/sunflower/vm/EvmHostImpl.kt
@@ -0,0 +1,65 @@
+package org.tdf.sunflower.vm
+
+import org.tdf.common.types.Uint256
+import org.tdf.common.util.HashUtil
+import org.tdf.common.util.HexBytes
+import org.tdf.evm.Digest
+import org.tdf.evm.EvmHost
+import org.tdf.sunflower.facade.RepositoryReader
+import java.math.BigInteger
+
+val sha3 = Digest {
+        src: ByteArray, srcPos: Int, srcLen: Int,
+        dst: ByteArray, dstPos: Int ->
+    HashUtil.sha3(src, srcPos, srcLen, dst, dstPos)
+}
+
+class EvmHostImpl(private val backend: Backend, private val rd: RepositoryReader): EvmHost {
+    override val digest: Digest
+        get() = sha3
+
+    override fun getBalance(address: ByteArray): BigInteger {
+        return backend.getBalance(HexBytes.fromBytes(address)).value
+    }
+
+    override fun setBalance(address: ByteArray, balance: BigInteger) {
+        backend.setBalance(HexBytes.fromBytes(address), Uint256.Companion.of(balance))
+    }
+
+    override fun getStorage(address: ByteArray, key: ByteArray): ByteArray {
+        return backend.dbGet(
+            HexBytes.fromBytes(address),
+            HexBytes.fromBytes(key)
+        ).bytes
+    }
+
+    override fun setStorage(address: ByteArray, key: ByteArray, value: ByteArray) {
+        backend.dbSet(
+            HexBytes.fromBytes(address),
+            HexBytes.fromBytes(key),
+            HexBytes.fromBytes(value)
+        )
+    }
+
+    override fun getCode(addr: ByteArray): ByteArray {
+        return backend.getCode(HexBytes.fromBytes(addr)).bytes
+    }
+
+    override fun call(
+        caller: ByteArray,
+        receipt: ByteArray,
+        input: ByteArray,
+        value: BigInteger,
+        delegate: Boolean
+    ): ByteArray {
+        TODO("Not yet implemented")
+    }
+
+    override fun drop(address: ByteArray) {
+        TODO("Not yet implemented")
+    }
+
+    override fun create(caller: ByteArray, value: BigInteger, createCode: ByteArray): ByteArray {
+        TODO("Not yet implemented")
+    }
+}
--- facade/src/main/java/org/tdf/sunflower/vm/VMExecutor.java
+++ facade/src/main/java/org/tdf/sunflower/vm/VMExecutor.java
@@ -8,9 +8,14 @@ import org.tdf.common.types.Uint256;
 import org.tdf.common.util.ByteUtil;
 import org.tdf.common.util.HashUtil;
 import org.tdf.common.util.HexBytes;
+import org.tdf.evm.EvmCallData;
+import org.tdf.evm.EvmContext;
+import org.tdf.evm.Interpreter;
 import org.tdf.lotusvm.Module;
 import org.tdf.lotusvm.ModuleInstance;
-import org.tdf.lotusvm.runtime.*;
+import org.tdf.lotusvm.runtime.Memory;
+import org.tdf.lotusvm.runtime.ResourceFactory;
+import org.tdf.lotusvm.runtime.StackAllocator;
 import org.tdf.sunflower.facade.RepositoryReader;
 import org.tdf.sunflower.state.Address;
 import org.tdf.sunflower.state.BuiltinContract;
@@ -31,12 +36,11 @@ public class VMExecutor {
     public static final int MAX_LABELS = MAX_FRAMES * 4;
     public static final int MAX_CALL_DEPTH = 8;
     public static final Cache<HexBytes, byte[]> CACHE =
-            CacheBuilder
-                    .newBuilder()
-                    .weigher((k, v) -> ((byte[]) v).length + ((HexBytes) k).size())
-                    .maximumWeight(1024L * 1024L * 8L) // 8mb cache for contracts
-                    .build();
-
+        CacheBuilder
+            .newBuilder()
+            .weigher((k, v) -> ((byte[]) v).length + ((HexBytes) k).size())
+            .maximumWeight(1024L * 1024L * 8L) // 8mb cache for contracts
+            .build();
 
 
     public VMExecutor(RepositoryReader rd, Backend backend, CallData callData, long gasLimit) {
@@ -92,8 +96,8 @@ public class VMExecutor {
 
         if (callData.getCallType() == CallType.CREATE) {
             contractAddress = HashUtil.calcNewAddrHex(
-                    callData.getCaller().getBytes(),
-                    callData.getTxNonceAsBytes()
+                callData.getCaller().getBytes(),
+                callData.getTxNonceAsBytes()
             );
             callData.setTo(contractAddress);
         }
@@ -109,13 +113,20 @@ public class VMExecutor {
 
 
         return new VMResult(
-                limit.getGas(),
-                contractAddress,
-                result,
-                Collections.emptyList(),
-                fee
+            limit.getGas(),
+            contractAddress,
+            result,
+            Collections.emptyList(),
+            fee
         );
     }
+
+    private static final byte[] WASM_MAGIC = {0x00, 0x61, 0x73, 0x6d};
+
+    private boolean isWasm(byte[] bytes) {
+        return Arrays.equals(bytes, 0, WASM_MAGIC.length, WASM_MAGIC, 0, WASM_MAGIC.length);
+    }
+
 
     @SneakyThrows
     public byte[] executeInternal() {
@@ -140,24 +151,35 @@ public class VMExecutor {
                 byte[] code;
                 // contract constructor/call arguments
                 byte[] data;
+                // if call context is evm, else web assembly
+                boolean isWasm;
+
                 HexBytes receiver = callData.getTo();
                 boolean create = callData.getCallType() == CallType.CREATE;
 
                 // if this is contract deploy, should create contract account
                 if (create) {
+                    isWasm = isWasm(callData.getData().getBytes());
+
                     // increase sender nonce
                     long n = backend.getNonce(callData.getCaller());
-                    try (Module tmpModule = Module.create(callData.getData().getBytes())) {
-                        // validate module
-                        ModuleValidator.INSTANCE.validate(tmpModule, false);
 
-                        code = WBI.dropInit(callData.getData().getBytes());
-                        data = WBI.extractInitData(tmpModule);
+                    if (isWasm) {
+                        try (Module tmpModule = Module.create(callData.getData().getBytes())) {
+                            // validate module
+                            ModuleValidator.INSTANCE.validate(tmpModule, false);
 
-                        // increase nonce here to avoid conflicts
-                        backend.setNonce(callData.getCaller(), n + 1);
-                        backend.setCode(receiver, HexBytes.fromBytes(code));
+                            code = WBI.dropInit(callData.getData().getBytes());
+                            data = WBI.extractInitData(tmpModule);
+                            backend.setCode(receiver, HexBytes.fromBytes(code));
+                        }
+                    } else {
+                        code = callData.getData().getBytes();
+                        data = ByteUtil.EMPTY_BYTE_ARRAY;
                     }
+
+                    // increase nonce here to avoid conflicts
+                    backend.setNonce(callData.getCaller(), n + 1);
                 } else {
                     HexBytes hash = backend.getContractHash(receiver);
                     // this is a transfer transaction
@@ -167,6 +189,7 @@ public class VMExecutor {
                         code = CACHE.get(backend.getContractHash(receiver), () -> backend.getCode(receiver).getBytes());
                     }
                     data = callData.getData().getBytes();
+                    isWasm = isWasm(code);
                 }
 
                 Objects.requireNonNull(code);
@@ -184,121 +207,149 @@ public class VMExecutor {
                 if (code.length == 0)
                     return ByteUtil.EMPTY_BYTE_ARRAY;
 
-                DBFunctions dbFunctions = new DBFunctions(backend, callData.getTo());
-
-                Hosts hosts = new Hosts()
-                        .withTransfer(
-                                backend,
-                                callData.getTo()
-                        )
-                        .withReflect(new Reflect(this))
-                        .withContext(new ContextHost(backend, callData))
-                        .withDB(dbFunctions)
-                        .withEvent(backend, callData.getTo());
-
-
-                try (
-                        StackAllocator stack =
-                                ResourceFactory.createStack(MAX_STACK_SIZE, MAX_FRAMES, MAX_LABELS);
-                        Memory mem = ResourceFactory.createMemory();
-                        Module module = Module.create(code);
-                ) {
-                    ModuleInstance instance =
-                            ModuleInstance
-                                    .builder()
-                                    .stackAllocator(stack)
-                                    .module(module)
-                                    .memory(mem)
-                                    .hooks(Collections.singleton(limit))
-                                    .hostFunctions(hosts.getAll())
-                                    .build();
-
-                    Abi abi = module.getCustomSections()
-                            .stream().filter(x -> x.getName().equals(WBI.ABI_SECTION_NAME))
-                            .findFirst()
-                            .map(x -> Abi.fromJson(new String(x.getData(), StandardCharsets.UTF_8)))
-                            .get();
-
-
-                    long[] rets;
-                    WBI.InjectResult r;
-
-                    r = WBI.inject(create, abi, instance, HexBytes.fromBytes(data));
-
-                    // payable check
-                    boolean payable = r.getEntry() != null && r.getEntry().isPayable();
-                    if (!payable && !callData.getValue().isZero())
-                        throw new RuntimeException("function " + r.getName() + " is not payable");
-
-                    if (!r.getExecutable())
-                        return ByteUtil.EMPTY_BYTE_ARRAY;
-
-                    rets = instance.execute(r.getName(), r.getPointers());
-                    List<Object> results = new ArrayList<>();
-                    List<Abi.Entry.Param> outputs;
-
-                    if (create) {
-                        outputs = Optional
-                                .ofNullable(abi.findConstructor())
-                                .map(x -> x.outputs)
-                                .orElse(Collections.emptyList());
-                    } else {
-                        outputs = abi.findFunction(x -> x.name.equals(r.getName())).outputs;
-                        if (outputs == null)
-                            outputs = Collections.emptyList();
-                    }
-
-
-                    // extract result
-                    for (int i = 0; i < rets.length; i++) {
-                        SolidityType type = outputs.get(i).type;
-                        switch (type.getName()) {
-                            case "uint8":
-                            case "uint16":
-                            case "uint32":
-                            case "uint64": {
-                                results.add(BigInteger.valueOf(rets[i]));
-                                break;
-                            }
-                            case "uint":
-                            case "uint256": {
-                                Uint256 u = (Uint256) WBI.peek(instance, (int) rets[i], WbiType.UINT_256);
-                                results.add(u.getValue());
-                                break;
-                            }
-                            case "string": {
-                                String s = (String) WBI.peek(instance, (int) rets[i], WbiType.STRING);
-                                results.add(s);
-                                break;
-                            }
-                            case "address": {
-                                HexBytes addr = (HexBytes) WBI.peek(instance, (int) rets[i], WbiType.ADDRESS);
-                                results.add(addr.getBytes());
-                                break;
-                            }
-                            default: {
-                                if (type.getName().endsWith("]") || type.getName().endsWith(")")) {
-                                    throw new RuntimeException("array or tuple is not supported");
-                                }
-
-                                if (type.getName().startsWith("bytes")) {
-                                    HexBytes bytes = (HexBytes) WBI.peek(instance, (int) rets[i], WbiType.BYTES);
-                                    results.add(bytes.getBytes());
-                                }
-                                break;
-                            }
-                        }
-                    }
-
-                    if (outputs.size() > 0) {
-                        return Abi.Entry.Param.encodeList(outputs, results.toArray());
-                    }
-
-                    return ByteUtil.EMPTY_BYTE_ARRAY;
-                }
+                if (isWasm)
+                    return executeWasm(create, code, data);
+                else
+                    return executeEvm(create, code, data);
             }
             default:
                 throw new UnsupportedOperationException();
+        }
+    }
+
+    private byte[] executeEvm(boolean create, byte[] code, byte[] input) {
+        EvmCallData evmCallData = new EvmCallData(
+            callData.getCaller().getBytes(),
+            callData.getTo().getBytes(),
+            callData.getValue().getValue(),
+            input,
+            code
+        );
+
+        EvmContext ctx = new EvmContext();
+        EvmHostImpl host = new EvmHostImpl(backend, rd);
+
+        Interpreter interpreter = new Interpreter(host, ctx, evmCallData, null);
+        interpreter.execute();
+
+        if (create) {
+            backend.setCode(callData.getTo(), HexBytes.fromBytes(interpreter.getRet()));
+        }
+        return create ? ByteUtil.EMPTY_BYTE_ARRAY : interpreter.getRet();
+    }
+
+    private byte[] executeWasm(boolean create, byte[] code, byte[] data) {
+        DBFunctions dbFunctions = new DBFunctions(backend, callData.getTo());
+
+        Hosts hosts = new Hosts()
+            .withTransfer(
+                backend,
+                callData.getTo()
+            )
+            .withReflect(new Reflect(this))
+            .withContext(new ContextHost(backend, callData))
+            .withDB(dbFunctions)
+            .withEvent(backend, callData.getTo());
+
+
+        try (
+            StackAllocator stack =
+                ResourceFactory.createStack(MAX_STACK_SIZE, MAX_FRAMES, MAX_LABELS);
+            Memory mem = ResourceFactory.createMemory();
+            Module module = Module.create(code);
+        ) {
+            ModuleInstance instance =
+                ModuleInstance
+                    .builder()
+                    .stackAllocator(stack)
+                    .module(module)
+                    .memory(mem)
+                    .hooks(Collections.singleton(limit))
+                    .hostFunctions(hosts.getAll())
+                    .build();
+
+            Abi abi = module.getCustomSections()
+                .stream().filter(x -> x.getName().equals(WBI.ABI_SECTION_NAME))
+                .findFirst()
+                .map(x -> Abi.fromJson(new String(x.getData(), StandardCharsets.UTF_8)))
+                .get();
+
+
+            long[] rets;
+            WBI.InjectResult r;
+
+            r = WBI.inject(create, abi, instance, HexBytes.fromBytes(data));
+
+            // payable check
+            boolean payable = r.getEntry() != null && r.getEntry().isPayable();
+            if (!payable && !callData.getValue().isZero())
+                throw new RuntimeException("function " + r.getName() + " is not payable");
+
+            if (!r.getExecutable())
+                return ByteUtil.EMPTY_BYTE_ARRAY;
+
+            rets = instance.execute(r.getName(), r.getPointers());
+            List<Object> results = new ArrayList<>();
+            List<Abi.Entry.Param> outputs;
+
+            if (create) {
+                outputs = Optional
+                    .ofNullable(abi.findConstructor())
+                    .map(x -> x.outputs)
+                    .orElse(Collections.emptyList());
+            } else {
+                outputs = abi.findFunction(x -> x.name.equals(r.getName())).outputs;
+                if (outputs == null)
+                    outputs = Collections.emptyList();
+            }
+
+
+            // extract result
+            for (int i = 0; i < rets.length; i++) {
+                SolidityType type = outputs.get(i).type;
+                switch (type.getName()) {
+                    case "uint8":
+                    case "uint16":
+                    case "uint32":
+                    case "uint64": {
+                        results.add(BigInteger.valueOf(rets[i]));
+                        break;
+                    }
+                    case "uint":
+                    case "uint256": {
+                        Uint256 u = (Uint256) WBI.peek(instance, (int) rets[i], WbiType.UINT_256);
+                        results.add(u.getValue());
+                        break;
+                    }
+                    case "string": {
+                        String s = (String) WBI.peek(instance, (int) rets[i], WbiType.STRING);
+                        results.add(s);
+                        break;
+                    }
+                    case "address": {
+                        HexBytes addr = (HexBytes) WBI.peek(instance, (int) rets[i], WbiType.ADDRESS);
+                        results.add(addr.getBytes());
+                        break;
+                    }
+                    default: {
+                        if (type.getName().endsWith("]") || type.getName().endsWith(")")) {
+                            throw new RuntimeException("array or tuple is not supported");
+                        }
+
+                        if (type.getName().startsWith("bytes")) {
+                            HexBytes bytes = (HexBytes) WBI.peek(instance, (int) rets[i], WbiType.BYTES);
+                            results.add(bytes.getBytes());
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (outputs.size() > 0) {
+                return Abi.Entry.Param.encodeList(outputs, results.toArray());
+            }
+
+            return ByteUtil.EMPTY_BYTE_ARRAY;
         }
     }
 }
