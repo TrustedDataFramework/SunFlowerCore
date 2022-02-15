a1c65ad9c3c887107ee130510fe62e521e9fa7bd
zhuyingjie <m6567fc@outlook.com>
2022-02-15 20:52:17 +0800

    fix

--- sunflower-core/src/main/java/org/tdf/sunflower/sync/SyncManager.kt
+++ sunflower-core/src/main/java/org/tdf/sunflower/sync/SyncManager.kt
@@ -514,7 +514,7 @@ class SyncManager(
                     var n = System.currentTimeMillis()
                     if (succeed.contains(it.hash)) return@l1
                     val p = toWrites[it.hashPrev] ?: repo.reader.use { rd -> rd.getBlockByHash(it.hashPrev) }
-                    val res = repo.reader.use { rd -> engine.validator.validate(rd, b, p!!) }
+                    val res = repo.reader.use { rd -> engine.validator.validate(rd, it, p!!) }
                     log.debug("validate block consume ${(System.currentTimeMillis() - n) / 1000.0}s")
                     log.debug("new block validate result = {}", res.success)
                     if (!res.success) {
